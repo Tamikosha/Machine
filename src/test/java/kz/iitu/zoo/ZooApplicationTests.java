13f1ac06a44be4b986687c4e1984de6588030e76
Tamikosha <56810672+Tamikosha@users.noreply.github.com>
2024-11-15 16:15:15 +0500

    init commin

---
+++ src/test/java/kz/iitu/zoo/ZooApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.zoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
